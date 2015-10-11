9902388d69b2b93c0a5662b3c0ed14a529e72350
davioliveirajr <davioliveiraj@hotmail.com>
2015-10-10 22:32:53 -0300

    Limpeza: Retirada de arquivos grandes e inúteis.'

--- projeto01/myClass_/includes/components/app/app.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('myClass', ['fsCordova'])
-.controller('AppController', 
-  function($scope, CordovaService) {
-    CordovaService.ready.then(function() {
-      // Cordova is ready
-      console.log("Cordova is ready");
-    });
-});
